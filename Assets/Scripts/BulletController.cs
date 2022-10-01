16b6d2954a395795dccc65b54d9caaae0ae75bea
Meliara <wesley.d.man@hotmail.com>
2022-10-01 22:54:22 +0200

    attempt 1 of bullet explosion
    part and attempt 1 of making the bullets explode visually

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    float bulletTtl = 5;
+    float bulletTtl = 1;
+    [SerializeField]
+    GameObject bulletExplosion;
+    Transform explosionPosition;
 
     // Update is called once per frame
     void Update()
     {
+        explosionPosition = GameObject.FindGameObjectWithTag("Bullet").transform;
         bulletTtl -= Time.deltaTime;
         if (bulletTtl <= 0)
         {
+            GameObject e = Instantiate(bulletExplosion, explosionPosition);
             Destroy(gameObject);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        GameObject e = Instantiate(bulletExplosion, explosionPosition);
         Destroy(gameObject);
     }
 }
